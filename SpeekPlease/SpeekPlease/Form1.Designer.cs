cf337855afaa3194d3851f1a759f2c0c365b75d9
Nebojša Bogosavljević <58832485+TheKing032@users.noreply.github.com>
2021-04-11 22:04:30 +0200

    Upload

---
+++ SpeekPlease/SpeekPlease/Form1.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace SpeekPlease
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.btnSpeak = new System.Windows.Forms.Button();
+            this.btnResume = new System.Windows.Forms.Button();
+            this.btnPause = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.btnMode = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 84);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(538, 263);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // btnSpeak
+            // 
+            this.btnSpeak.Location = new System.Drawing.Point(12, 353);
+            this.btnSpeak.Name = "btnSpeak";
+            this.btnSpeak.Size = new System.Drawing.Size(130, 57);
+            this.btnSpeak.TabIndex = 1;
+            this.btnSpeak.Text = "Read";
+            this.btnSpeak.UseVisualStyleBackColor = true;
+            this.btnSpeak.Click += new System.EventHandler(this.btnSpeak_Click);
+            // 
+            // btnResume
+            // 
+            this.btnResume.Location = new System.Drawing.Point(148, 353);
+            this.btnResume.Name = "btnResume";
+            this.btnResume.Size = new System.Drawing.Size(130, 57);
+            this.btnResume.TabIndex = 2;
+            this.btnResume.Text = "Resume";
+            this.btnResume.UseVisualStyleBackColor = true;
+            this.btnResume.Click += new System.EventHandler(this.btnResume_Click);
+            // 
+            // btnPause
+            // 
+            this.btnPause.Location = new System.Drawing.Point(284, 353);
+            this.btnPause.Name = "btnPause";
+            this.btnPause.Size = new System.Drawing.Size(130, 57);
+            this.btnPause.TabIndex = 3;
+            this.btnPause.Text = "Pause";
+            this.btnPause.UseVisualStyleBackColor = true;
+            this.btnPause.Click += new System.EventHandler(this.btnPause_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(420, 353);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(130, 57);
+            this.btnStop.TabIndex = 4;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // btnMode
+            // 
+            this.btnMode.Location = new System.Drawing.Point(12, 5);
+            this.btnMode.Name = "btnMode";
+            this.btnMode.Size = new System.Drawing.Size(538, 40);
+            this.btnMode.TabIndex = 5;
+            this.btnMode.Text = "Change Mod";
+            this.btnMode.UseVisualStyleBackColor = true;
+            this.btnMode.Click += new System.EventHandler(this.btnMode_Click);
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.Info;
+            this.label1.Location = new System.Drawing.Point(12, 48);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(538, 29);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Current Mode: Reading";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(565, 420);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnMode);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnPause);
+            this.Controls.Add(this.btnResume);
+            this.Controls.Add(this.btnSpeak);
+            this.Controls.Add(this.richTextBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button btnSpeak;
+        private System.Windows.Forms.Button btnResume;
+        private System.Windows.Forms.Button btnPause;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Button btnMode;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ SpeekPlease/SpeekPlease/Form1.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Speech;
+using System.Speech.Synthesis;
+using System.Speech.AudioFormat;
+using System.Speech.Synthesis.TtsEngine;
+using System.Speech.Recognition;
+using System.Speech.Recognition.SrgsGrammar;
+
+
+
+namespace SpeekPlease
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public string FielText
+        {
+            get { return richTextBox1.Text; }
+            set { richTextBox1.Text = value; }
+        }
+        SpeechSynthesizer speechSynthesizerObj;
+        bool mode = true;
+        static SpeechRecognitionEngine engine;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            speechSynthesizerObj = new SpeechSynthesizer();
+            btnResume.Enabled = false;
+            btnPause.Enabled = false;
+            btnStop.Enabled = false;
+            Console.WriteLine(richTextBox1.Text);
+            richTextBox1.Clear();
+        }
+        static void engine_SpeechRecognized(object ob, SpeechRecognizedEventArgs e)
+        {
+            Form1 form1 = new Form1();
+            form1.richTextBox1.Text = e.Result.Text;
+            Console.WriteLine(e.Result.Text);
+            
+        }
+        static Grammar MyGrammar()
+        {
+            DictationGrammar defaultDictationGrammar = new DictationGrammar();
+            defaultDictationGrammar.Name = "default dictation";
+            defaultDictationGrammar.Enabled = true;
+
+            DictationGrammar spellingDictationGrammar =
+                new DictationGrammar("grammar:dictation#spelling");
+            spellingDictationGrammar.Name = "spelling dictation";
+            spellingDictationGrammar.Enabled = true;
+
+            DictationGrammar customDictationGrammar =
+                new DictationGrammar("grammar:dictation");
+            customDictationGrammar.Name = "question dictation";
+            customDictationGrammar.Enabled = true;
+
+            return defaultDictationGrammar;
+        }
+        private void btnSpeak_Click(object sender, EventArgs e)
+        {
+            if (mode == true)
+            {
+                speechSynthesizerObj.Dispose();
+                if (richTextBox1.Text != "")
+                {
+                    speechSynthesizerObj = new SpeechSynthesizer();
+                    speechSynthesizerObj.SpeakAsync(richTextBox1.Text);
+                    btnPause.Enabled = true;
+                    btnStop.Enabled = true;
+                }
+            }
+            else
+            {
+                
+
+                engine = new SpeechRecognitionEngine();
+                engine.SetInputToDefaultAudioDevice();
+                Grammar g = MyGrammar();
+                engine.LoadGrammar(g);
+                engine.RecognizeAsync(RecognizeMode.Multiple);
+                engine.SpeechRecognized += new EventHandler<SpeechRecognizedEventArgs>(engine_SpeechRecognized);
+                richTextBox1.Text = Console.ReadLine();
+                
+            }
+        }
+
+        private void btnResume_Click(object sender, EventArgs e)
+        {
+            if(speechSynthesizerObj != null)
+            {
+                if(speechSynthesizerObj.State == SynthesizerState.Paused)
+                {
+                    speechSynthesizerObj.Resume();
+                    btnResume.Enabled = false;
+                    btnSpeak.Enabled = true;
+                }
+            }
+        }
+  
+        private void btnPause_Click(object sender, EventArgs e)
+        {
+            if(speechSynthesizerObj != null)
+            {
+                if(speechSynthesizerObj.State == SynthesizerState.Speaking)
+                {
+                    speechSynthesizerObj.Pause();
+                    btnResume.Enabled = true;
+                    btnSpeak.Enabled = false;
+                }
+            }
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            if(speechSynthesizerObj != null)
+            {
+                speechSynthesizerObj.Dispose();
+                btnSpeak.Enabled = true;
+                btnResume.Enabled = false;
+                btnPause.Enabled = false;
+                btnStop.Enabled = false;
+            }
+        }
+        private void changeModToSpeakign()
+        {
+            label1.Text = "Current mode: Reading";
+            btnSpeak.Text = "Read";
+        }
+
+        private void btnMode_Click(object sender, EventArgs e)
+        {
+            if (label1.Text == "Current mode: Speaking")
+            {
+                changeModToSpeakign();
+                mode = true;
+            }
+            else
+            {
+                label1.Text = "Current mode: Speaking";
+                btnSpeak.Text = "Speak";
+                mode = false;
+            }
+        }
+    }
+}
